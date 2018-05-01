fed9be76c8d3a607d455f95e1ddc0c7648017db7
dev-joeld <joel.dalit@xero.com>
2018-05-02 10:31:38 +1200

    search

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    term: '',
+    img: '',
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      term: event.target.value,
+    })
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const apiKey = 'gKvbPlPFx9WoglD03wWua4EU2ucElFxZ';
+    const url = `http://api.giphy.com/v1/gifs/search?q=${this.state.term}&api_key=${apiKey}`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          term: '',
+          img: data.data[0].images.original.url
+        })
+      })
+      .catch(e => console.log('error', e));
+  };
+
   render() {
+
+    const { term, img } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={term} onChange={this.handleChange} />
+          <button type="submit">Search</button>
+        </form>
+        <img src={img} height="200"/>
       </div>
     );
   }
